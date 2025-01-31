70b0fde65ea9518260c040923d0c5ebee1686fdd
Ax-47 <armingLwnza@gmail.com>
2025-01-31 21:47:35 +0700

    sdasda

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // ปิด Image Optimization เพราะใช้ GitHub Pages
   },
-  basePath: true ? '/https://ax-47.github.io/nuclear.github.io/' : '',
-  assetPrefix: true ? 'https://ax-47.github.io/nuclear.github.io/' : '',
+  basePath: '/https://ax-47.github.io/nuclear.github.io/',
+  assetPrefix: 'https://ax-47.github.io/nuclear.github.io/' ,
 };
 
 export default nextConfig;
