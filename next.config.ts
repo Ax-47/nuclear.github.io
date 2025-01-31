b3a9196635599cd6b37765f473d2989348516428
Ax-47 <armingLwnza@gmail.com>
2025-01-31 21:46:15 +0700

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
+  images: {
+    unoptimized: true, // ปิด Image Optimization เพราะใช้ GitHub Pages
+  },
+  basePath: true ? '/https://ax-47.github.io/nuclear.github.io/' : '',
+  assetPrefix: true ? 'https://ax-47.github.io/nuclear.github.io/' : '',
 };
 
 export default nextConfig;
